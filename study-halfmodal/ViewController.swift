0f03884ff52598a85a1b2b97a682068437dc06fa
Yo Higashida <y.higashida0226+app@gmail.com>
2022-02-03 13:26:22 +0900

    add: modal

--- study-halfmodal/ViewController.swift
+++ study-halfmodal/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func modalButton () {
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "NextViewController") as! NextViewController
+        if let sheet = vc.sheetPresentationController {
+            // モーダルのサイズを指定する
+            sheet.detents = [.medium(), .large()]
+            
+            // 上部の短いバーを表示する（デフォルトは非表示）
+            sheet.prefersGrabberVisible = true
+            
+            // 元のViewControllerも操作できるようにする
+            sheet.largestUndimmedDetentIdentifier = .medium
+        }
+        present(vc, animated: true, completion: nil)
+    }
 }
-
